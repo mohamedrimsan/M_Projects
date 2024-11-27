4f8778431bdd6bac72cc3dc117ea7306742eb54a
Mohamed Rimsan <159031220+mohamedrimsan@users.noreply.github.com>
2024-11-27 19:38:29 +0530

    Practice

--- Java Mini Projects/GradePointAverageCalculator/GPACalculator.java
+++ Java Mini Projects/GradePointAverageCalculator/GPACalculator.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+1import java.util.Scanner;
 
 public class GPACalculator {
     public static void main(String[] args) {
---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+<script>
+console.log("Hello")
+
+alert("Welcome Back") 
+
+let amount = 0;
+
+amount = 10000;
+
+console.log("Your bill amount is Rs." + amount)
+</script>
